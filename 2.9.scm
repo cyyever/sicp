bf90eb123c97f2221fe5cedf9444a724c4427c06
cyy <cyyever@outlook.com>
2017-07-23 18:48:46 +0800

    new exc

---
+++ 2.9.scm
@@ -0,0 +1,33 @@
+(define (make-interval a b) (cons a b))
+
+(define (lower-bound x) (car x))
+
+(define (upper-bound x) (cdr x))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval x 
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))))
+
+(define (width x)
+  (/ (- (upper-bound x) (lower-bound x) ) 2))
+
+(define interval-a (make-interval 1 3))
+
+(define interval-b (make-interval 1 4))
+
+(width interval-a)
+
+(width interval-b)
+
+(width (mul-interval interval-a interval-b))
+
+(width (div-interval interval-a interval-b))
