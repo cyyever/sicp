38e761b7efe4b8f319ab170e30cda11765af2397
cyy <cyyever@outlook.com>
2017-07-26 23:08:26 +0800

    exc

---
+++ 2.32.scm
@@ -0,0 +1,7 @@
+(define (subsets s)
+    (if (null? s)
+            (list s)
+	          (let ((rest (subsets (cdr s))))
+		            (append rest (map (lambda (set) (cons (car s) set)) rest)))))
+
+(subsets (list 1 2 3))
