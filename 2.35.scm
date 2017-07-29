1b84fe07ecad6c8252bc28e6649251609fbe6e07
cyy <cyyever@outlook.com>
2017-07-29 10:18:06 +0800

    缩进代码

--- 2.35.scm
+++ 2.35.scm
@@ -1,16 +1,16 @@
 (define (accumulate op initial sequence)
   (if (null? sequence)
-      initial
-      (op (car sequence)
-          (accumulate op initial (cdr sequence)))))
+    initial
+    (op (car sequence)
+	(accumulate op initial (cdr sequence)))))
 
 (define (count-leaves t)
-  (accumulate  + 0 (map (lambda (x) 
-(if (pair? x)
-(count-leaves x)
-1)) t)
-  )) 
+  (accumulate + 0 
+	      (map (lambda (x) 
+		     (if (pair? x)
+		       (count-leaves x)
+		       1)) t)))
 
 (define x (cons (list 1 2) (list 3 4)))
 
- (count-leaves x)
+(count-leaves x)
