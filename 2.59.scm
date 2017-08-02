42ff6f8ca44e74fe8df1b099164f98c0aefd91c6
cyyever <cyyever@outlook.com>
2017-08-02 22:27:51 +0800

    new exc

---
+++ 2.59.scm
@@ -0,0 +1,16 @@
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+	((equal? x (car set)) true)
+	(else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+    set
+    (cons x set)))
+
+(define (union-set set1 set2) 
+  (if (null? set1) 
+    set2
+    (adjoin-set (car set1) (union-set (cdr set1) set2))))
+
+(union-set (list 1 2) (list 2 3))
